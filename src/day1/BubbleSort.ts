8b2fd391caafc80277965211bfc02bb54c6f7c98
alireza akbarzadeh <devtools95@gmail.com>
2025-02-23 11:39:31 +0330

    feat: solve bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let index = 0; index < arr.length; index++) {
+        for (let j = 0; j < arr.length - 1 - index; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
