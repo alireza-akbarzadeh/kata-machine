214b1da7b33527660e2dc293245a16b5159024ed
alireza akbarzadeh <devtools95@gmail.com>
2025-02-23 10:07:23 +0330

    feat; solve tow ball question

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,15 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    //
+    let justAmount = Math.floor(Math.sqrt(breaks.length));
+    let index = justAmount;
+    for (; index < breaks.length; index += justAmount) {
+        if (breaks[index]) {
+            break;
+        }
+    }
+    index -= justAmount;
+    for (let j = 0; j < justAmount && index < breaks.length; j++, index++) {
+        if (breaks[index]) return index;
+    }
+    return -1;
 }
